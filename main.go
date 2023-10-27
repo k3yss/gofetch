d5e909e0565a644e1b3cdf01f663f48a68087d86
Rishi Kumar <rsi.dev17@gmail.com>
2023-10-27 17:25:27 +0530

    Add gopher

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("         ,_---~~~~~----._         ")
+    fmt.Println("  _,,_,*^____      _____``*g*\"*, ")
+    fmt.Println(" / __/ /'     ^.  /      \\ ^@q   f ")
+    fmt.Println("[  @f | @))    |  | @))   l  0 _/  ")
+    fmt.Println(" `\\   \\~____ / __ \\_____/    \\   ")
+    fmt.Println("  |           _l__l_           I   ")
+    fmt.Println("  }          [______]           I  ")
+    fmt.Println("  ]            | | |            |  ")
+    fmt.Println("  ]             ~ ~             |  ")
+    fmt.Println("  |                            |   ")
+    fmt.Println("   |                           |   ")
+}
